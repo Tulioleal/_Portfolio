c045692224c215adddd17ac34a18c308120fd2de
Tulio <tulioleal13@gmail.com>
2021-02-03 15:54:18 -0300

    Fixed

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -6,7 +6,8 @@ const ProjectsList = {
         "repo": "https://github.com/Tulioleal/_AppConsultorio",
     },
     Splitter : {
-        "content": "Splitter is a very simple to use app, created to split bills between you and your friends in a party, reunion or even trip",
+        "title" : "Splitter",
+        "content": "Splitter is a very simple to use app, created to split bills between you and your friends in a party, reunion or even trip. (this is a work in the early stages of development)",
         "href": "/",
         "repo": "/",
     }
