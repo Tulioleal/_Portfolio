460a432ff7330f53426c218d43aed887fb950d58
Tulio <tulioleal13@gmail.com>
2021-02-15 18:44:09 -0300

    add Splitter url

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -7,8 +7,8 @@ const ProjectsList = {
     },
     Splitter : {
         "title" : "Splitter",
-        "content": "Splitter is a very simple to use app, created to split bills between you and your friends in a party, reunion or even trip. (this is a work in the early stages of development)",
-        "href": "/",
+        "content": "Splitter is a very simple to use app, created to split bills between you and your friends in a party, reunion or even trip. (this is a work in the early stages of development, currently you can visualize it only in it's mobil version)",
+        "href": "https://splitter.tulioleal.vercel.app/",
         "repo": "https://github.com/Tulioleal/_Splitter",
     }
 }
