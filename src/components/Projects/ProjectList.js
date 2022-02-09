36d20e27123be2142a1188b5289c5d6ae22f27d3
Tulio Leal <tuliolealdev@gmail.com>
2022-02-09 10:31:21 -0300

    replace json with js for better img handdle

---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,34 @@
+import Consul from '../../images/Home1-min.jpg'
+import Splitter from '../../images/Splitter.png'
+import Nggamdu from '../../images/Nggamdu.jpg'
+import MiTiendaOficial from '../../images/mto.jpg'
+
+export default {
+    "ConsultorioApp" : {
+        "title" : "ConsultorioApp",
+        "content": "A Single-page application created in 2020, focused in simplicity, developed as a tool for medical history and consultation of gynecologic and obstetric patients.",
+        "href": "https://consultorioapp.herokuapp.com",
+        "repo": "https://github.com/Tulioleal/_AppConsultorio",
+        "img" : Consul,
+    },
+    "Splitter" : {
+        "title" : "Splitter",
+        "content": "Splitter is a very simple to use app, created to split bills between you and your friends in a party, reunion or even trip. (Currently you can visualize it only in it's mobile version).",
+        "href": "https://splitter.tulioleal.vercel.app/",
+        "repo": "https://github.com/Tulioleal/_Splitter",
+        "img": Splitter
+    },
+    "Nggamdu" : {
+        "title" : "Nggàm dù",
+        "content": "Spider divination with somié, Cameroon. A project developed together with Thomas Saraceno's team, an Argentinian Artist in Berlin.",
+        "href": "https://www.nggamdu.org/",
+        "img": Nggamdu
+    },
+    "MiTiendaOficial" : {
+        "title" : "Mi tienda oficial",
+        "content": "App developted to create a customizable site for Essen entrepreneurs.",
+        "href": "https://mitiendaoficial.com.ar/",
+        "img": MiTiendaOficial
+    }
+
+}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import Style from './Projects.module.scss'
-import Consul from '../../images/Home1-min.jpg'
-import Splitter from '../../images/Splitter.png'
-import ProjectList from './ProjectList.json';
+import ProjectList from './ProjectList';
 
 import Fade from 'react-reveal/Fade'
 
 const Project = ( props ) => {
   return (
-
       <div className={Style.card}>
           <a href={props.href} target='_blank' rel="noreferrer" >
                 <img src={props.src} alt={props.alt}/> 
@@ -18,8 +15,17 @@ const Project = ( props ) => {
               <h3>{props.title}</h3>
               <p>{props.content}</p>
               <div className={Style.links}>
-                  <a href={props.href} target='_blank' rel="noreferrer">Site</a>
-                  <a href={props.repo} target='_blank' rel="noreferrer">Repo</a>
+
+                  { props.href && (
+                    <a href={props.href} target='_blank' rel="noreferrer">Site</a>
+                    )
+                  }
+
+                  { props.repo && (
+                      <a href={props.repo} target='_blank' rel="noreferrer">Repo</a>
+                    )
+                  }
+
               </div>
           </section>
       </div>
@@ -32,22 +38,20 @@ function Projects() {
         <div className={Style.img2}>
             <Fade >
               <h2>Projects</h2>
-              <Project
-                href = {ProjectList.ConsultorioApp.href}
-                alt = {ProjectList.ConsultorioApp.title}
-                src = {Consul}
-                repo = {ProjectList.ConsultorioApp.repo}
-                title = {ProjectList.ConsultorioApp.title}
-                content = {ProjectList.ConsultorioApp.content}
-              />
-              <Project
-                href = {ProjectList.Splitter.href}
-                alt = {ProjectList.Splitter.title}
-                src = {Splitter}
-                repo = {ProjectList.Splitter.repo}
-                title = {ProjectList.Splitter.title}
-                content = {ProjectList.Splitter.content}
-              />
+
+              {
+                Object.keys(ProjectList).map( project => (
+                  <Project
+                    href = {ProjectList[project].href}
+                    alt = {ProjectList[project].title}
+                    src = {ProjectList[project].img}
+                    repo = {ProjectList[project].repo}
+                    title = {ProjectList[project].title}
+                    content = {ProjectList[project].content}
+                  />
+                ))
+              }
+
             </Fade>
         </div>
     )
